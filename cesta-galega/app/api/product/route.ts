004d0529aa51b3b9138c880fa92a7eec2aa858e4
Rodrigo Iglesias Nieto <iglnierod@gmail.com>
2025-11-04 23:37:24 +0100

    feat(api): crea endpoint api/product

---
+++ cesta-galega/app/api/product/route.ts
@@ -0,0 +1,60 @@
+import prisma from '@/app/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const businessId = searchParams.get('businessId');
+
+    const where = businessId ? { businessId: Number(businessId) } : {};
+
+    const products = await prisma.product.findMany({
+      where,
+      include: {
+        business: true,
+        categories: true,
+      },
+    });
+
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, description, businessId, categoryIds } = body;
+
+    if (!name || !description || !businessId || !categoryIds) {
+      return NextResponse.json(
+        { error: 'Name, description, businessId y categoryIds son necesarios ' },
+        { status: 400 }
+      );
+    }
+
+    const newProduct = await prisma.product.create({
+      data: {
+        name,
+        description,
+        business: { connect: { id: businessId } },
+        categories: categoryIds
+          ? {
+              connect: categoryIds.map((id: number) => ({ id })),
+            }
+          : undefined,
+      },
+      include: {
+        business: true,
+        categories: true,
+      },
+    });
+
+    return NextResponse.json({ newProduct }, { status: 201 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: 'Error al crear el producto' }, { status: 500 });
+  }
+}
