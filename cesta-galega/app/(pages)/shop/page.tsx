b06573dee3240c307b0a76cb966a1b0d83e7e130
Rodrigo Iglesias Nieto <iglnierod@gmail.com>
2025-11-28 12:15:40 +0100

    chore: entrega pequeñas actualizaciones reunion proyecto

--- cesta-galega/app/(pages)/shop/page.tsx
+++ cesta-galega/app/(pages)/shop/page.tsx
@@ -1,3 +1,12 @@
-export default function ShopPage() {
+import { getAuthTokenDecoded, isCookieValid, JwtPayloadUser } from '@/app/lib/auth';
+
+export default async function ShopPage() {
+  let loggedIn = false;
+  let decoded: JwtPayloadUser | undefined;
+  const cookieValid = await isCookieValid();
+  if (cookieValid) {
+    decoded = (await getAuthTokenDecoded()) as JwtPayloadUser;
+    loggedIn = true;
+  }
   return <>Shop page works!</>;
 }
